531e2f772f1b5bd2971ce163fcc917865f754371
MQuintanaV <maritza.quintana.vasquez@gmail.com>
2023-07-21 11:57:40 -0400

    practica consolidacion games

---
+++ src/components/juegos.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="juegos">
+        <h1>componente juegos</h1>
+        <div class="contenedorPadre">
+            <div class="card" style="width: 18rem;">
+                    <img src="../assets/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+        </div>                
+
+</div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default{
+    name:'juegos',
+    data:function(){
+        return{
+
+        }
+    },
+    methods:{
+        consumirApi:function(){
+
+            let url='https://api.rawg.io/api/platforms?key=bc355598c96a47c59cb5b8e8efbce746'
+            axios(url)
+            .then(respuesta =>{
+                console.log(respuesta);
+               
+            })
+            .catch(error=>{
+                console.log(error);
+            });
+
+        },
+    },
+    created(){
+        this.consumirApi();
+    }
+}
+</script>
+<style scoped>
+#juegos{
+    background-color: #F3E5F5;
+}
+.contenedorPadre{
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Lita de Juegos Disponibles</h1>
+<juegos></juegos>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import juegos from '@/components/juegos.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    juegos
   }
 }
 </script>
